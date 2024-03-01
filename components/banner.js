b60aea2778ca7d000caad6933f5f03f90011eb52
Manasa Thota <manasa.manu57@gmail.com>
2024-03-01 23:09:48 +0000

    props concepts

--- components/banner.js
+++ components/banner.js
@@ -1,13 +1,13 @@
 import styles from './banner.module.css';
 
-const Banner = () => {
+const Banner = ({headerText}) => {
     return(
         <header className="row mb-4">
             <div className="col-5">
                 <img src="./GloboLogo.png" alt="logo" className={styles.logo}/>
             </div>
             <div className="col-7 mt-5">
-                <div className={styles.subtitleStyle} >Providing houses all over the world</div>
+                <div className={styles.subtitleStyle} >{headerText}</div>
             </div> 
         </header>
     );
